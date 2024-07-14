f219ad1a52181be63fd431bac41276af7373b4ac
shanwc1972 <166612646+shanwc1972@users.noreply.github.com>
2024-07-14 22:33:12 +1000

    Final code plus comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 // Global variables for the form elements
 const submitEl = document.querySelector('#searchbutton');
 const citySelectionEl = document.querySelector('#city-selection');
+const forecastEl = document.querySelector('#five-day-forecast');
 
+//The following function checks local storage for existing search history and populates the search history buttons
 function retrieveSearchHistory(){
     let arrHistory = JSON.parse(localStorage.getItem('SearchHistory'));
     if (arrHistory != null) {
@@ -15,11 +17,13 @@ function retrieveSearchHistory(){
             historyItem.setAttribute('id', `${arrHistory[i]}`);
             historyItem.textContent = arrHistory[i];
             searchHistoryEl.appendChild(historyItem);
+            //Create a listener for each spawned button
             historyItem.addEventListener('click', handleCitySearch);
         }       
     }
 }
 
+//The following function pushes the most recent search to the top of the search history updating the local storage accordingly
 function updateSearchHistory(strCity) {
     let arrHistory = JSON.parse(localStorage.getItem('SearchHistory'));
     if(arrHistory == null){
@@ -37,11 +41,13 @@ function updateSearchHistory(strCity) {
     retrieveSearchHistory();
 }
 
+//The following function populates the current city selection from data gathered by the API 
 function updateCitySelection(callResult) {
     
     let strDate = new Date(callResult.list[0].dt*1000);
     strDate = strDate.toLocaleDateString();
     let strCityAndDate = callResult.city.name + "(" + strDate + ")";
+    //Add a weather icon to header
     let iconurl = "http://openweathermap.org/img/w/" + callResult.list[0].weather[0].icon + ".png";
     const wicon = document.createElement("img");
     wicon.setAttribute('src', iconurl);
@@ -57,6 +63,7 @@ function updateCitySelection(callResult) {
     citySelectionEl.children[1].children[2].textContent = "Humidity: " + strHumidity + " %";
 }
 
+//The following function populates the 5 day forecast section using an an array of weather objects
 function updateForecast(callResult) {
 
     let arrForecastData = [];
@@ -66,6 +73,9 @@ function updateForecast(callResult) {
     let strHumidity = '';
     let strWicon = '';
     objWeather = {};
+    
+    //Display the 5 day forecast section
+    forecastEl.style.display = "grid";
     
     //Build an array of weather objects comprising temp, wind speed and humidityfor next five days
     for(let i=0; i<5; i++) {
@@ -84,7 +94,6 @@ function updateForecast(callResult) {
         arrForecastData.push(objWeather);
         objWeather= {};
     }
-    console.log(arrForecastData);
 
     let cardEl = null;
     let day = {};
@@ -103,15 +112,14 @@ function updateForecast(callResult) {
 
 }
 
+//This function is the handler for all button click events
 function handleCitySearch(event) {
     //From the city input text, query the API and capture the output
     event.preventDefault();
 
-    console.log(event);
-    console.log(event.target.id);
-
     const cityInput = document.querySelector('#city');
 
+    //Determine if the button is the search or one of the history buttons and then process acccordingly
     if(event.target.id != "searchbutton") {
         cityInput.value = event.target.id
     } else{
@@ -122,10 +130,12 @@ function handleCitySearch(event) {
         }
 
     const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput.value}&appid=c5b8bb91e48454aab366682512e22626&units=metric`
+    //Log our query URL to the console.log to ensure that the API call is correct 
     console.log(queryURL);
 
     fetch(queryURL)
         .then(function (response) {
+            //Provide an error alert if the there is API data
             if (response.status !== 200) {
                 alert("City not recognized");
             }
@@ -135,7 +145,7 @@ function handleCitySearch(event) {
             return response.json();
         })
         .then(function (callResult) {
-            
+            //Process the API data if valid
             updateCitySelection(callResult);
             updateForecast(callResult);
             updateSearchHistory(callResult.city.name);
@@ -147,6 +157,10 @@ function handleCitySearch(event) {
         cityInput.value = "";
 }
 
+//Event listener for the search button 
 submitEl.addEventListener('click', handleCitySearch);
 
+//Hide the 5 day forecast by default
+forecastEl.style.display = "none";
+//Check and display any search history items
 retrieveSearchHistory();
